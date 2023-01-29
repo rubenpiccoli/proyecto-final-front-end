ee3a218e10b0ed43f113afb9e81fb42f256cadad
rubenpiccoli <rubendariopiccoli@gmail.com>
2023-01-29 14:35:01 -0300

    se agrega navbar y se realiza con javascript informacion adicional al pasar por arriba de los iconos de contacto

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+
+let telefono=document.getElementById('phone')
+telefono.addEventListener('mouseenter', function()
+{
+    document.getElementById('conctato_js').innerHTML= "3447-4454522"
+})
+
+let correo=document.getElementById('mail')
+    correo.addEventListener('mouseenter', function()
+    {
+        document.getElementById('conctato_js').innerHTML= "hola@sitioincreible.com"
+    })
+    
+    let facebook=document.getElementById('facebook')
+    facebook.addEventListener('mouseenter', function()
+    {
+        document.getElementById('conctato_js').innerHTML= "hola@sitioincreible.facebook.com"
+    })
+    
+    let localizacion=document.getElementById('location')
+    localizacion.addEventListener('mouseenter', function()
+    {
+        document.getElementById('conctato_js').innerHTML= "San Martín 4544 - Paraná Entre Rios"
+    })
+   
+ 
